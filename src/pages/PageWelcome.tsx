b2dfdd706a7937599c047a3588dd5a95022cab99
Edward Tanguay (windows) <edward@tanguay.info>
2024-02-11 18:30:07 +0100

    showing need for zod

--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -1,10 +1,27 @@
 import { useContext } from "react";
 import { AppContext } from "../AppContext";
+import { IMember } from "../interfaces";
 
 export const PageWelcome = () => {
-	const { message } = useContext(AppContext);
-	console.log(111, message);
+	const { message, members } = useContext(AppContext);
 	return (
-		<p>{message}</p>
-	)
-}
+		<>
+			<p>{message}</p>
+			<h2 className="mt-4 text-xl">Members</h2>
+			<ul className="list-disc ml-6">
+				{members.map((member:IMember) => {
+					return (
+						<>
+							{member.id < 10 && (
+<li key={member.id}>{member.name}</li>
+							):(
+<li key={member.id.substring(0,3)}>{member.name}</li>
+						)}
+						</>
+					) 
+				})}
+			</ul>
+		</>
+	);
+};
+ 
